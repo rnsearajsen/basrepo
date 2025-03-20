e5ea9bf1983e21a0fee1d211021ef2c140009158
Naveen raj RajaSenthil <rajasenthil.naveenaraj@cognizant.com>
2025-03-20 18:49:10 +0000

    Carousel Change1

--- UI5 Handson/webapp/controller/Carousel.controller.js
+++ UI5 Handson/webapp/controller/Carousel.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel'
+    
 ], function(Controller, JSONModel) {
     'use strict';
     return Controller.extend("ux400.training.ux400handson.controller.Carousel", {
